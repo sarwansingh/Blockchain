c0d2e1f04eed743c2bdd9a48457369cbdeaadb2d
sarwansingh <sarwan_singh@yahoo.com>
2021-08-10 10:55:14 +0530

    update

---
+++ code/ON085/bank_dApp/bank.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.4.25;
+
+contract Bank{
+	int bal;
+    constructor () public
+    {
+    	bal=10;
+    }
+    function getBalance() view public returns (int){
+    	return bal;
+    }
+    function withdraw(int amt) public
+    {
+    	bal =bal -amt;
+    }
+    function deposit(int amt)public 
+    {
+    	bal = bal+amt;
+    }
+}
